c3665a8f2f218e98140575537082c53b17cac28f
gennalop <gennalop@espol.edu.ec>
2025-07-11 17:00:39 -0500

    icon for gui

--- gui/screens/home_buttons.py
+++ gui/screens/home_buttons.py
@@ -1,17 +1,22 @@
 import tkinter as tk
+from tkinter import PhotoImage
 from gui import style
 
 def create_buttons_panel(self, r, c):
     actions_frame = tk.Frame(self, bg=style.BACKGROUND)
     actions_frame.grid(row=r, column=c, sticky="nsew")
     actions_frame.rowconfigure(1, weight=1)
-    actions_frame.columnconfigure(2, weight=1) 
+    actions_frame.columnconfigure(3, weight=1) 
 
-    tk.Button(actions_frame, text="⬆️", bg=style.BUTTONS_T2_BG, width=3, command=self.open_file).grid(row=0, column=0)
-    tk.Button(actions_frame, text="Limpiar", bg=style.BUTTONS_T2_BG, command=self.clean_panels).grid(row=0, column=1, padx=4)
+    self.down_icon = PhotoImage(file="gui/images/down.png")
+    self.up_icon = PhotoImage(file="gui/images/up.png")
+
+    tk.Button(actions_frame,  image=self.up_icon, bg=style.BUTTONS_T2_BG,  width=30, height=20, command=self.open_file).grid(row=0, column=0)
+    #tk.Button(actions_frame,  image=self.down_icon, bg=style.BUTTONS_T2_BG,  width=30, height=20, command=self.open_file).grid(row=0, column=1, padx=4)
+    tk.Button(actions_frame, text="Limpiar", bg=style.BUTTONS_T2_BG, command=self.clean_panels).grid(row=0, column=2, padx=4)
     self.boton_toggle_live = tk.Button(actions_frame, text="Live: OFF", bg=style.BUTTON_OFF_BG, command=self.toggle_live)
-    self.boton_toggle_live.grid(row=0, column=3, padx=4)
-    tk.Button(actions_frame, text="Ejecutar todo", bg=style.BUTTONS_T1_BG, command=self.analizar_todo).grid(row=0, column=4)
+    self.boton_toggle_live.grid(row=0, column=4, padx=4)
+    tk.Button(actions_frame, text="Ejecutar todo", bg=style.BUTTONS_T1_BG, command=self.analizar_todo).grid(row=0, column=5)
 
     buttons_frame = tk.Frame(actions_frame, bg=style.BACKGROUND)
     buttons_frame.grid(row=2, column=0, columnspan=5, sticky="w", padx=0)
